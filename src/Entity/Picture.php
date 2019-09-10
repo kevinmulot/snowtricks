3cac2d083ff11e6c0fd1e78f772a71cbd299ade6
Kevin Mulot <kevin.mulot@gmail.com>
2019-09-10 14:30:18 +0200

    Created picture entity

---
+++ src/Entity/Picture.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PictureRepository")
+ */
+class Picture
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Trick", inversedBy="picture")
+     * @ORM\JoinColumn(name="trick_id", referencedColumnName="id")
+     */
+    private $trick;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     * @return Picture
+     */
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+}
