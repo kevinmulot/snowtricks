4e56f507fc60a299713ca3e275d18f8783bee56e
Kevin Mulot <kevin.mulot@gmail.com>
2019-09-24 05:21:56 +0200

    Profile picture form added

---
+++ src/Form/ProfileFormType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Profile;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class ProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Profile Picture',
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/pjpeg',
+                            'image/png'
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image (jpg, jpeg, png)',
+                    ])
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Submit'
+            ]);;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Profile::class,
+        ]);
+    }
+}
