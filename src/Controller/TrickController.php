e8a83934f019201a476c8121983048edb14d00dd
Kevin Mulot <kevin.mulot@gmail.com>
2019-10-19 02:51:09 +0200

    Added edit and delete methods

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -3,15 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
-use App\Entity\Profile;
 use App\Entity\Trick;
-use App\Entity\User;
+use App\Entity\Picture;
 use App\Form\CommentFormType;
 use App\Form\TrickFormType;
+use App\Service\FileUploader;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * Class TrickController
@@ -25,22 +26,20 @@ class TrickController extends AbstractController
     private $em;
 
     /**
-     * @Route("/tricks", name="tricks")
-     */
-    public function index()
-    {
-        return $this->render('/trick/tricks.html.twig', [
-            'controller_name' => 'TricksController',
-        ]);
-    }
-
-    /**
      * TrickController constructor.
      * @param ObjectManager $em
      */
     public function __construct(ObjectManager $em)
     {
         $this->em = $em;
+    }
+
+    /**
+     * @Route("/tricks", name="tricks")
+     */
+    public function index()
+    {
+        return $this->render('/trick/tricks.html.twig');
     }
 
     /**
@@ -83,6 +82,48 @@ class TrickController extends AbstractController
             $this->em->flush();
         }
         $comments = $trick->getComment();
-        return $this->render('trick/trick.view.html.twig', ['trick' => $trick, 'commentForm' => $form->createView(), 'comments' => $comments]);
+        $pictures = $trick->getPicture();
+        $videos = $trick->getVideo();
+        return $this->render('trick/trick.view.html.twig', ['trick' => $trick, 'commentForm' => $form->createView(), 'comments' => $comments, 'pictures' => $pictures, 'videos' => $videos]);
+    }
+
+    /**
+     * @Route("/tricks/delete/{id}", name="trick_delete")
+     * @param Trick $trick
+     */
+    public function deleteTrick(Trick $trick, FileUploader $fileUploader)
+    {
+        $pictures = $trick->getPicture();
+        foreach ($pictures as $picture) {
+            /*@object Picture $picture */
+            $name = $picture->getName();
+            $fileUploader->remove($name);
+        }
+        $this->em->remove($trick);
+        $this->em->flush();
+        return $this->redirect('/');
+    }
+
+    /**
+     * @Route("/trick/edit/{id}", name="trick_edit")
+     * @param Trick $trick
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function edit(Request $request, trick $trick)
+    {   $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $trickForm = $this->createForm(TrickFormType::class, $trick);
+        $trickForm->handleRequest($request);
+
+        if ($trickForm->isSubmitted() && $trickForm->isValid()) {
+            $trick->setDateUpdate(new \DateTime('now'));
+            $this->em->flush();
+            return $this->redirectToRoute('trick_view', array('id' => $trick->getId()));
+        }
+        $pictures = $trick->getPicture();
+        $videos = $trick->getVideo();
+        return $this->render('trick/trick.edit.html.twig', [
+            'trickForm' => $trickForm->createView(),
+            'trick' => $trick, 'pictures' => $pictures, 'videos' => $videos]);
     }
 }
