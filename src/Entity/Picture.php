c36150244dc0a8492e04ccb4b4c9cd2493cbdf9b
Kevin Mulot <kevin.mulot@gmail.com>
2019-10-19 02:41:51 +0200

    Added picture management system

--- src/Entity/Picture.php
+++ src/Entity/Picture.php
@@ -3,13 +3,8 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
-
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PictureRepository")
- *  @Vich\Uploadable
  */
 class Picture
 {
@@ -19,109 +14,54 @@ class Picture
      * @ORM\Column(type="integer")
      */
     private $id;
-
-    /**
-     * @ORM\Column(type="string", length=40)
-     */
-    private $type;
-
     /**
      * @ORM\Column(type="string", length=255)
-     *
-     * @var string
      */
-    private $imageName;
-
-    /**
-     * NOTE: This is not a mapped field of entity metadata, just a simple property.
-     *
-     * @Vich\UploadableField(mapping="tricks_image", fileNameProperty="imageName")
-     *
-     * @var File
-     */
-    private $imageFile;
-
-    /**
-     * @ORM\Column(type="datetime")
-     *
-     * @var \DateTime
-     */
-    private $updatedAt;
-
+    private $name;
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Trick", inversedBy="picture")
-     * @ORM\JoinColumn(name="trick_id", referencedColumnName="id", nullable=false)
+     *
      */
     private $trick;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $statut;
 
     public function __construct()
     {
-        $this->updatedAt = new \DateTime('now');
+        $this->statut = "normal";
     }
 
     public function getId(): ?int
     {
         return $this->id;
     }
-
-    public function getType(): ?string
+    public function getName()
     {
-        return $this->type;
+        return $this->name;
     }
-
-    public function setType(string $type): self
+    public function setName($name)
     {
-        $this->type = $type;
-
+        $this->name = $name;
         return $this;
     }
-
-    public function getTrick(): ?int
+    public function getTrick(): ?trick
     {
         return $this->trick;
     }
-
-    public function setTrick(?int $trick): self
+    public function setTrick(?trick $trick): self
     {
         $this->trick = $trick;
-
         return $this;
     }
-
-    /**
-     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
-     * of 'UploadedFile' is injected into this setter to trigger the update. If this
-     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
-     * must be able to accept an instance of 'File' as the bundle will inject one here
-     * during Doctrine hydration.
-     *
-     * @param null|File $imageFile
-     * @return Picture
-     */
-    public function setImageFile(?File $imageFile): Picture
+    public function getStatut(): ?string
     {
-        $this->imageFile = $imageFile;
-
-        if ($this->imageFile instanceof UploadedFile) {
-            // It is required that at least one field changes if you are using doctrine
-            // otherwise the event listeners won't be called and the file is lost
-            $this->updatedAt = new \DateTimeImmutable();
-        }
+        return $this->statut;
+    }
+    public function setStatut(string $statut): self
+    {
+        $this->statut = $statut;
         return $this;
-    }
-
-    public function getImageFile(): ?File
-    {
-        return $this->imageFile;
-    }
-
-    public function setImageName(?string $imageName): void
-    {
-        $this->imageName = $imageName;
-    }
-
-    public function getImageName(): ?string
-    {
-        return $this->imageName;
     }
 }
