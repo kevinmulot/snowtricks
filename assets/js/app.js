31b762928d535de302b6e9596e7290345004eb5a
Kevin Mulot <kevin.mulot@gmail.com>
2019-10-30 13:38:11 +0100

    'load more' management for Tricks Medias  comments / Smooth scrolling

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,10 +7,10 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require("../css/app.css");
-require('@fortawesome/fontawesome-free/css/all.min.css');
-require('@fortawesome/fontawesome-free/js/all.js');
+require("@fortawesome/fontawesome-free/css/all.min.css");
+require("@fortawesome/fontawesome-free/js/all.js");
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-const $ = require('jquery');
+const $ = require("jquery");
 require("bootstrap");
 console.log("Hello Webpack Encore! Edit me in assets/js/app.js");
 
@@ -18,6 +18,99 @@ console.log("Hello Webpack Encore! Edit me in assets/js/app.js");
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
+$(document).ready(function () {
+    $("[data-toggle='popover']").popover();
+});
+
+//******************* Load More Tricks **********************//
+$(function () {
+    $(".holdertrick").slice(0, 5).show();
+    $("#loadMoreTricks").on('click', function (e) {
+        e.preventDefault();
+        $(".holdertrick:hidden").slice(0, 5).slideDown();
+        if ($(".holdertrick:hidden").length === 0) {
+            $("#loadMoreTricks").fadeOut('slow');
+        }
+    });
+});
+
+
+//******************* Load More Media **********************//
+$(function () {
+    $(".holder").slice(0, 6).show();
+    $("#loadMoreMedia").on('click', function (e) {
+        e.preventDefault();
+        $(".holder:hidden").slice(0, 6).slideDown();
+        if ($(".holder:hidden").length === 0) {
+            $("#loadMoreMedia").fadeOut('slow');
+        }
+    });
+});
+//******************* Load More Comments **********************//
+$(function () {
+    $(function () {
+        $(".holdercomments").slice(0, 10).show();
+        $("#loadMoreComments").on('click', function (e) {
+            e.preventDefault();
+            $(".holdercomments:hidden").slice(0, 10).slideDown();
+
+            if ($(".holdercomments:hidden").length === 0) {
+                $("#loadMoreComments").fadeOut('slow');
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    $("#show").click(function () {
+        $("#medias").toggle();
+    });
+});
+
+$(window).resize(function () {
+    var win = $(this); //this = window
+    if (win.width() >= 768) {
+        $("#medias").show();
+    }
+});//run on every window resize
+
+$(document).ready(function () {
+    var counter = 0;
+    $("#loadMoreTricks").click(function () {
+        counter++;
+        if (counter >= 2) {
+            $(".uptricks").show();
+        }
+    });
+});
+
+$(document).ready(function () {
+    $("#showlist").click(function () {
+        $(".tricklist").show();
+        $("#showlist").fadeOut('slow');
+    });
+});
+
+$(function () {
+    /**
+     * Smooth scrolling to page anchor on click
+     **/
+    $("a[href*='#']:not([href='#'])").click(function () {
+        if (
+            location.hostname == this.hostname
+            && this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")
+        ) {
+            var anchor = $(this.hash);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
+            if (anchor.length) {
+                $("html, body").animate({scrollTop: anchor.offset().top}, 1500);
+            }
+        }
+    });
+});
+
+$(".openmodal").click(function () {
+    var href = $(this).data("href");
+    $("#imagemodal img").attr("src", href);
+    $("#imagemodal").modal("show");
 });
