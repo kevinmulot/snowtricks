e890b7b116dee79854bfde5d3fa8794c78f628d1
Kevin Mulot <kevin.mulot@gmail.com>
2019-11-05 02:39:41 +0100

    Code climate

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -31,14 +31,10 @@ class RegistrationFormType extends AbstractType
             ->add('username', TextType::class,['required' => true])
             ->add('email', EmailType::class, ['required' => true])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'label' => 'Password',
                 'mapped' => false,
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
+                    new NotBlank(['message' => 'Please enter a password',]),
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
--- src/Form/ResetPasswordFormType.php
+++ src/Form/ResetPasswordFormType.php
@@ -28,8 +28,6 @@ class ResetPasswordFormType extends AbstractType
         $builder
             ->add('username', TextType::class, ['required' => true])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'label' => 'Password',
                 'mapped' => false,
                 'required' => true,
